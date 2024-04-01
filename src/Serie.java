54dceb7aa22275ac865e314299ca74d086057ef0
shayan <shayanemoradi@gmail.com>
2024-04-01 11:02:04 +0330

    ass

--- src/Serie.java
+++ src/Serie.java
@@ -22,13 +22,14 @@ public class Serie {
         String nummbersUnSortedX = nummbersUnSortedBuilder.toString();
         System.out.println(nummbersUnSortedX);
 
-        int maxLenghtOfseriesOfAlltime = 1;
-        int maxLenghtOfseriesTemp = 1;
+        int maxLenghtOfseriesOfAlltime = 0;
+        int maxLenghtOfseriesTemp = 0;
         int beginngOfBigestIncreasingSequence = 0;
         int endOfBigestIncreasingSequence = 0;
 
         int previousNumber = Integer.MIN_VALUE;
         int tempBeginningHolder = -1;
+        int numberOfIncreasingSeries=0;
 
         // Iterate over each number in the input string
         for (int i = 0; i < nummbersUnSortedX.length(); i++) {
@@ -44,8 +45,10 @@ public class Serie {
             // Check if the current number is greater than the previous number
             if (currentNumber >= previousNumber) {
                 maxLenghtOfseriesTemp++;
+                //just beginning of bigesst series
                 if (tempBeginningHolder == -1) {
                     tempBeginningHolder = currentNumber;
+
                 }
 
                 // Check if the current increasing sequence is the longest so far
@@ -55,7 +58,11 @@ public class Serie {
                     endOfBigestIncreasingSequence = currentNumber;
 
                 }
-            } else {
+            } //either end of ass serie
+            else {
+                //carefull if we just have inceasing output 1
+                numberOfIncreasingSeries++;
+
                 maxLenghtOfseriesTemp = 1;
                 tempBeginningHolder = currentNumber;
             }
@@ -63,10 +70,16 @@ public class Serie {
             // Update the previous number
             previousNumber = currentNumber;
         }
+        //carefull if we just have inceasing output 1
+        if (numberOfIncreasingSeries==0) numberOfIncreasingSeries=1;
+
 
         System.out.println("Max length of increasing series: " + maxLenghtOfseriesOfAlltime);
         System.out.println("Starting number of the longest increasing series: " + beginngOfBigestIncreasingSequence);
         System.out.println("Ending number of the longest increasing series: " + endOfBigestIncreasingSequence);
+        int lenght = endOfBigestIncreasingSequence-beginngOfBigestIncreasingSequence;
+        System.out.println("length  of the longest increasing series:(diff) " + lenght);
+        System.out.println("numbers of increasing series: "+numberOfIncreasingSeries);
     }
 }
 
